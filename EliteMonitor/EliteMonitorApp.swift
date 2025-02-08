e67189abfc84aabf8b6e3f7f2e9b7caebbb6390b
Andrew Childs <andrew.childs@bibo.com.ph>
2025-02-08 13:49:40 +0900

    Initial Commit

---
+++ EliteMonitor/EliteMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EliteMonitorApp.swift
+//  EliteMonitor
+//
+//  Created by Andrew Childs on 2025/02/08.
+//
+
+import SwiftUI
+
+@main
+struct EliteMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
